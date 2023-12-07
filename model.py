780a9849584e35384e259da88c35357c5a7f431b
Kaustubh Dighe <kaustubhdighe1@gmail.com>
2023-12-07 16:46:23 -0500

    modified model to accept text and images together

--- model.py
+++ model.py
@@ -6,8 +6,10 @@ from torch import nn
 class CliPortModel(nn.Module):
     def __init__(self):
         super(CliPortModel, self).__init__()
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
         self.language_model = BertModel.from_pretrained('bert-base-uncased')
         self.visual_model = models.resnet50(pretrained=True)
+        
         # Modify the last layer of ResNet to match BERT's feature size
         self.visual_model.fc = nn.Linear(self.visual_model.fc.in_features, self.language_model.config.hidden_size)
         self.classifier = nn.Sequential(
@@ -15,26 +17,26 @@ class CliPortModel(nn.Module):
             nn.ReLU(),
             nn.Linear(512, 128),
             nn.ReLU(),
-            nn.Linear(128, 2)  # Assuming 2D manipulation actions
+            nn.Linear(128, 4)  # Output 4 coordinates (start and end)
         )
 
-    def forward(self, input_ids, attention_mask, visual_input):
-        language_output = self.language_model(input_ids=input_ids, attention_mask=attention_mask)
-        visual_output = self.visual_model(visual_input)
+    def forward(self, inputs):
+        tokenized_input = self.tokenizer(
+                            inputs['text'], 
+                            return_tensors="pt", 
+                            padding=True, 
+                            truncation=True, 
+                            max_length=512
+                            )
+        language_output = self.language_model(
+                            input_ids=tokenized_input['input_ids'], 
+                            attention_mask=tokenized_input['attention_mask']
+                            )
+        visual_output = self.visual_model(inputs['image'].float())
         combined = torch.cat((language_output.pooler_output, visual_output), dim=1)
-        actions = self.classifier(combined)
-        return actions
+        coordinates = self.classifier(combined)
+        return coordinates
 
 if __name__ == '__main__':
-    # Example usage
     model = CliPortModel()
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-
-    # Dummy inputs
-    text = "Pick up the red block"
-    inputs = tokenizer(text, return_tensors="pt")
-    visual_input = torch.rand((1, 3, 224, 224))  # Random image
-
-    # Forward pass
-    actions = model(inputs['input_ids'], inputs['attention_mask'], visual_input)
-    print(actions)
+    print(model({'text': 'Pick up the red block', 'image': torch.rand((1, 3, 224, 224))}))
